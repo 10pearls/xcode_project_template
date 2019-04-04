5f01f8d5b015e2cd6f81a82cc5965a495250b5fd
Waleed Khan <waleed.khan@tenpearls.com>
2019-04-04 11:26:36 +0500

    Apply Fonts in TextField.

--- Project Templates/TenPearls/iOS Starter App.xctemplate/swift/Controls/TextField.swift
+++ Project Templates/TenPearls/iOS Starter App.xctemplate/swift/Controls/TextField.swift
@@ -7,6 +7,19 @@ class TextField: UITextField {
     override func awakeFromNib() {
         super.awakeFromNib()
         //Find a way to set up fonts
+        
+        setFont()
+    }
+    
+}
+
+extension UITextField {
+    
+    func setFont() {
+        if let face = font?.fontDescriptor.object(forKey: .face) as? String, let pointSize = font?.pointSize  {
+            font = Fonts.font(face: face, fontSize: pointSize)
+        }
+        
     }
     
 }

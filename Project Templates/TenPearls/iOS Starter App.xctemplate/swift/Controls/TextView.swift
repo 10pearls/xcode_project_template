409b150a841b349e63dd106cb4ecdd0d834a875a
Waleed Khan <waleed.khan@tenpearls.com>
2019-04-04 11:31:56 +0500

    Added TextView Component

---
+++ Project Templates/TenPearls/iOS Starter App.xctemplate/swift/Controls/TextView.swift
@@ -0,0 +1,29 @@
+// ___FILEHEADER___.
+
+import UIKit
+
+class TextView: UITextView {
+    
+    // MARK: - Life Cycle Methods
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        initialize()
+    }
+    
+    private func initialize() {
+        setFont()
+    }
+    
+}
+
+extension UITextView {
+    
+    func setFont() {
+        if let face = font?.fontDescriptor.object(forKey: .face) as? String, let pointSize = font?.pointSize  {
+            font = Fonts.font(face: face, fontSize: pointSize)
+        }
+        
+    }
+    
+}

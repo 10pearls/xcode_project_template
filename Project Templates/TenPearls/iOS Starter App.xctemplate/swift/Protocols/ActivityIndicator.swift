e7afdc45f1308a05c58889cefdde2e8640f78e54
Waleed Khan <waleed.khan@tenpearls.com>
2019-04-04 17:33:02 +0500

    Improvements in ActivityIndicator and ErrorHandling.

--- Project Templates/TenPearls/iOS Starter App.xctemplate/swift/Protocols/ActivityIndicator.swift
+++ Project Templates/TenPearls/iOS Starter App.xctemplate/swift/Protocols/ActivityIndicator.swift
@@ -9,3 +9,36 @@ protocol ActivityIndicator {
     func showLoaderWith(message: String)
     
 }
+
+extension UIView: ActivityIndicator {
+    
+    func showLoader() {
+        MBProgressHUD.showAdded(to: self, animated: true)
+    }
+
+    func hideLoader() {
+        MBProgressHUD.hide(for: self, animated: true)
+    }
+
+    func showLoaderWith(message: String) {
+        let hud = MBProgressHUD.showAdded(to: self, animated: true)
+        hud.label.text = message
+    }
+    
+}
+
+extension UIViewController: ActivityIndicator {
+    
+    func showLoader() {
+        self.view.showLoader()
+    }
+    
+    func hideLoader() {
+        self.view.hideLoader()
+    }
+    
+    func showLoaderWith(message: String) {
+        self.view.showLoaderWith(message: message)
+    }
+    
+}
--- Project Templates/TenPearls/iOS Starter App.xctemplate/swift/Protocols/ErrorHandling.swift
+++ Project Templates/TenPearls/iOS Starter App.xctemplate/swift/Protocols/ErrorHandling.swift
@@ -9,15 +9,15 @@ protocol ErrorHandling {
 }
 
 extension ErrorHandling where Self: ActivityIndicator {
-    
+
     func throwException(message: String) {
         let exception = NSException(name: NSExceptionName(rawValue: message), reason: nil, userInfo: nil)
         exception.raise()
     }
-    
+
     func handleError(_ error: ErrorConvertible?) {
         hideLoader()
-        
+
         if error?.errorCode() == 401 {
             forceLogoutUser()
             return
@@ -25,11 +25,12 @@ extension ErrorHandling where Self: ActivityIndicator {
         if let err = error {
             _ = Alert.show(title: "Error", message: err.errorMessage())
         }
-        
+
     }
-    
+
     func forceLogoutUser() {
         //Do Your Stuff here...
     }
-    
+
 }
+

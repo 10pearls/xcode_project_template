c8dd8add9a6aba090bb7d66856504f38a26e880a
asad.ali <asad.ali@tenpearls.com>
2019-04-09 06:53:33 -0400

    path updates

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ mkdir /usr/local/bin
 
 cd /usr/local/bin
 
-curl -O http://10.0.0.192/git/xcode_project_template/raw/template_binary/bin/xcode-seed
+curl -O http://10.0.0.192/git/xcode_project_template/raw/master/bin/xcode-seed
 
 chmod 700 xcode-seed
